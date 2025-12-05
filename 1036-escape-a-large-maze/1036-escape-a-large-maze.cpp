052bf92de9124c52417e86ea1039168f03036c6b
Ashish Kumar Choudhary <144052437+ashishkrchy@users.noreply.github.com>
2025-12-05 11:05:15 +0530

    Time: 1071 ms (10.6%), Space: 94.2 MB (27.65%) - LeetHub

--- 1036-escape-a-large-maze/1036-escape-a-large-maze.cpp
+++ 1036-escape-a-large-maze/1036-escape-a-large-maze.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
 
-    const int MAXN = 1000000;
-    const int LIMIT = 20000;  
+    const int LIMIT = 20000;
 
-    vector<int> dx = {0, 0, -1, 1};
-    vector<int> dy = {-1, 1, 0, 0};
+    bool bfs(vector<int>& start, vector<int>& target, unordered_set<string>& blockedSet) {
 
-    string getKey(int x, int y){
-        return to_string(x) + "_" + to_string(y);
-    }
+        vector<int> dx = {0, 0, -1, 1}; 
+        vector<int> dy = {-1, 1, 0, 0}; 
 
-    
-    bool bfs(vector<int>& start, vector<int>& end, unordered_set<string>& blockedSet) {
-
-        unordered_set<string> visited;
         queue<pair<int,int>> q;
+        unordered_set<string> visited;
+
+        auto makeKey = [&](int x, int y){
+            return to_string(x) + "_" + to_string(y);
+        };
 
         q.push({start[0], start[1]});
-        visited.insert(getKey(start[0], start[1]));
+        visited.insert(makeKey(start[0], start[1]));
 
-        while (!q.empty() && visited.size() <= LIMIT) {
+        while(!q.empty() && visited.size() <= LIMIT){
 
             auto [i, j] = q.front();
             q.pop();
 
-
-            if (i == end[0] && j == end[1])
+            if(i == target[0] && j == target[1])
                 return true;
 
-            for (int d = 0; d < 4; d++) {
+            for(int d = 0; d < 4; d++){
                 int x = i + dx[d];
                 int y = j + dy[d];
 
-                if (x >= 0 && y >= 0 && x < MAXN && y < MAXN) {
+                if(x < 0 || y < 0 || x >= 1000000 || y >= 1000000) 
+                    continue;
 
-                    string key = getKey(x, y);
+                string key = makeKey(x, y);
 
-                    if (!blockedSet.count(key) && !visited.count(key)) {
-                        visited.insert(key);
-                        q.push({x, y});
-                    }
+                if(!blockedSet.count(key) && !visited.count(key)){
+                    visited.insert(key);
+                    q.push({x, y});
                 }
             }
         }
@@ -52,11 +49,11 @@ public:
 
         unordered_set<string> blockedSet;
 
-
-        for (auto &b : blocked)
-            blockedSet.insert(getKey(b[0], b[1]));
-
-
+        for (auto &b : blocked) {
+            string key = to_string(b[0]) + "_" + to_string(b[1]);
+            blockedSet.insert(key);
+        }
+    
         return bfs(source, target, blockedSet) && bfs(target, source, blockedSet);
     }
 };
