8d5dda41674528c636931ffaeacda9767fc3fd66
Ashish Kumar Choudhary <144052437+ashishkrchy@users.noreply.github.com>
2025-12-05 08:44:36 +0530

    Time: 17 ms (62.58%), Space: 25.3 MB (57.2%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -5,34 +5,37 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        if(grid[0][0] == 1) return -1;
+        
+        if (grid[0][0] == 1 || grid[m - 1][n - 1] == 1)
+            return -1;
 
         vector<int> dx = {-1, 1, 0, 0, -1, 1, -1, 1};
         vector<int> dy = {0, 0, -1, 1, -1, 1, 1, -1};
-        
+
         vector<vector<int>> dist(m, vector<int>(n, -1));
-        queue<pair<int,int>> q;
+        queue<pair<int, int>> q;
 
         q.push({0,0});
         dist[0][0] = 1;
 
-        while(!q.empty()){
+        while (!q.empty()) {
             auto [i, j] = q.front();
             q.pop();
 
-            for(int d = 0; d < 8; d++){
+            if (i == m - 1 && j == n - 1)
+                return dist[i][j];
+
+            for (int d = 0; d < 8; d++) {
                 int x = i + dx[d];
                 int y = j + dy[d];
 
-                if(x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 0){
-                    grid[x][y] = 1;
+                if (x >= 0 && y >= 0 && x < m && y < n && grid[x][y] == 0 && dist[x][y] == -1) {
                     dist[x][y] = dist[i][j] + 1;
-                    q.push({x,y});
+                    q.push({x, y});
                 }
             }
         }
 
-        return dist[m - 1][n - 1];
-
+        return -1; 
     }
 };
