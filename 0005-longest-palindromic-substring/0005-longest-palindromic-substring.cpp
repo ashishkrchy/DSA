15bd7007a556648a7f0971c24cc805b5e6bbb1e7
Ashish Kumar Choudhary <144052437+ashishkrchy@users.noreply.github.com>
2025-12-25 20:09:54 +0530

    Time: 7 ms (86.27%), Space: 9.3 MB (78.71%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    int t[1001][1001];
     
-    bool solve(string &s, int i, int j){
-        if(i >= j) return 1;
-
-        if(t[i][j] != -1) return t[i][j];
-
-        if(s[i] == s[j]) return t[i][j] = solve(s, i+1, j-1);
-        return t[i][j] = 0;
+    void expand(string &s, int left, int right, int &start, int &maxLen) {
+        int n = s.size();
+        
+        while (left >= 0 && right < n && s[left] == s[right]) {
+            if (right - left + 1 > maxLen) {
+                maxLen = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
     }
 
     string longestPalindrome(string s) {
         int n = s.size();
-        int maxLen = 0;
-        int startingP = 0;
+        int start = 0, maxLen = 1;
 
-        memset(t, -1, sizeof(t));
-
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if (solve(s, i, j)) {
-                    if (maxLen < j - i + 1) {
-                        maxLen = j - i + 1;
-                        startingP = i;
-                    }
-                }
-            }
+        for (int i = 0; i < n; i++) {
+            // Odd length
+            expand(s, i, i, start, maxLen);
+            
+            // Even length
+            expand(s, i, i + 1, start, maxLen);
         }
-        return s.substr(startingP, maxLen);
+
+        return s.substr(start, maxLen);
     }
 };
