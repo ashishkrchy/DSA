f7d59969faaa3575f5b3f04a0194954d189d6c10
Ashish Kumar Choudhary <144052437+ashishkrchy@users.noreply.github.com>
2025-12-29 10:05:14 +0530

    Time: 210 ms (18.18%), Space: 99.5 MB (18.18%) - LeetHub

--- 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
+++ 1669-merge-in-between-linked-lists/1669-merge-in-between-linked-lists.cpp
@@ -1,47 +1,28 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
         
         ListNode* curr = list1;
+        ListNode* A = nullptr;
+        ListNode* B = nullptr;
 
-        ListNode* A = NULL;
-        ListNode* B = NULL;
+        int idx = 0;
 
-        int count = 0;
-
-        while(curr){
-            if(count == a - 1){
-                A = curr;
-            }
-            else if(count == b + 1){
-                B = curr;
-            }
-
+        while (curr) {
+            if (idx == a - 1) A = curr;
+            if (idx == b + 1) B = curr;
             curr = curr->next;
-            count++;
+            idx++;
         }
 
         A->next = list2;
 
-        if(B){
-            ListNode* temp = list2;
-            while(temp->next){
-                temp = temp->next;
-            }
-
-            if(B) temp->next = B;
+        curr = list2;
+        while (curr->next) {
+            curr = curr->next;
         }
+
+        curr->next = B;
 
         return list1;
     }
