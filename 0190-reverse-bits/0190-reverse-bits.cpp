208dbe7b12f60172b0b80fa3f8aa30c066066ab9
Ashish Kumar Choudhary <144052437+ashishkrchy@users.noreply.github.com>
2026-02-16 22:46:37 +0530

    Time: 4 ms (29.28%), Space: 7.8 MB (100%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    int reverseBits(int n) {
-
-        string str = "";
-        
-        while(n > 0){
-            str += (n % 2 == 0) ? '0' : '1';
-            n /= 2;
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        for (int i = 0; i < 32; i++) {
+            ans =  ans<<1;
+            if(n&1){
+                ans=ans|1;
+            }
+            n = n>>1;
         }
-
-        int m = str.size();
-
-        string temp(32- m, '0');
-
-        str += temp;
-
-        int ans = 0;
-
-        for(int i = 0; i < 32; i++){
-            ans += (str[i] == '1') ? pow(2, str.size() - 1 - i): 0;
-        }
-
         return ans;
     }
 };
