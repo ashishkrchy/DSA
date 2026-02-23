4670c2243c4e41414e8ca30f2111fbffc891d466
Ashish Kumar Choudhary <144052437+ashishkrchy@users.noreply.github.com>
2026-02-23 21:31:48 +0530

    Time: 0 ms (100%), Space: 41 MB (5.88%) - LeetHub

--- 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.cpp
+++ 3847-find-the-score-difference-in-a-game/3847-find-the-score-difference-in-a-game.cpp
@@ -6,41 +6,16 @@ public:
         int first = 0;
         int second = 0;
 
-        int turn = true;
+        bool activeFirst = true;   
 
         for(int i = 0; i < n; i++){
 
-            if(nums[i] % 2 != 0 && (i + 1) % 6 == 0){
+            if(nums[i] & 1) activeFirst = !activeFirst;
 
-                if(turn){
-                    first += nums[i];
-                }
-                else{
-                    second += nums[i];
-                }
+            if(i % 6 == 5) activeFirst = !activeFirst;
 
-            }
-            else if(nums[i] % 2 != 0 || (i + 1) % 6 == 0){
-
-                if(turn){
-                    second += nums[i];
-                }
-                else{
-                    first += nums[i];
-                }
-
-                turn = !turn;
-            }
-            else{
-
-                if(turn){
-                    first += nums[i];
-                }
-                else{
-                    second += nums[i];
-                }
-
-            }
+            if(activeFirst) first += nums[i];
+            else second += nums[i];
         }
 
         return first - second;
